1c4f9435ce0ed1935acb95b68c45b22ce32806fb
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-15 20:40:25 -0700

    created employee component

--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import employees from '../../employees.json';
+import React, { Component } from 'react';
 import Card from '../Card';
 
-class Employee extends React.Component {
+class Employee extends Component {
     render() {
         return (
             <div className="employee">
-                {employees.map(employee => (
+                {this.props.employees.map(employee => (
                     <Card key={employee.id} name={employee.name} role={employee.role} department={employee.department} image={employee.image}/>
                 ))}
             </div>
