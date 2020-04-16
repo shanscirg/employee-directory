9040ad7943938fa4bd404c26d4de14d1ad086c2f
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-15 20:39:23 -0700

    created card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,7 +4,7 @@ import image from "../../employee.png";
 class Card extends React.Component {
     render() {
         return (
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card" style={{ width: "18rem" }}>
                 <div className="card-body">
                     <h5 className="card-title">{this.props.name}</h5>
                     <img src={image} className="card-img-top" alt="img" />
