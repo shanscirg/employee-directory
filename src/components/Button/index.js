6eeb2ea4b8c9e867c9cea916d33f308ee8b308ab
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-15 20:41:47 -0700

    created button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = ({ onClick, children }) => {
+    return (
+        <div>
+            <button onClick={onClick}>
+                {children}
+            </button>
+        </div>
+    )
+}
+
+export default Button;
